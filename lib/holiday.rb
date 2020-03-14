c94139d882fb37cfe77d27a01239412f6479b33b
Christopher Cid <iamcid17@gmail.com>
2020-03-14 15:58:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   answer = []
-  
+  holiday_hash.each do |
 end
 
 
