fda48116864a5840cc514c5a1584639bedfe6a1d
Christopher Cid <iamcid17@gmail.com>
2020-03-14 15:54:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.to_s.capitalize!}:"
     data.each do |holiday, supply|
-      array = holiday.to_s.supply(" ")
+      array = holiday.to_s.split(" ")
       final_holiday = []
         array.each do |x|
           final_holiday << x.capitalize!
