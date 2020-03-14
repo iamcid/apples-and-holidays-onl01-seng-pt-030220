c653c315ca3a8565fc5d5a3e75a8dda9f5112a80
Christopher Cid <iamcid17@gmail.com>
2020-03-14 15:50:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.to_s.captialize!}:"
     data.each do |holiday, supply|
-      array = holiday
+      array = holiday.to_s.supply (" ")
 end
 
 def all_holidays_with_bbq(holiday_hash)
