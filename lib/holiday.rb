57b5b0e544693909f69132c298d975dffd32a5b1
Christopher Cid <iamcid17@gmail.com>
2020-03-14 15:52:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday = final_holiday.join(" ")  
       
       supply = supply.join(",")
-      
+      puts "#{"
 end
 
 def all_holidays_with_bbq(holiday_hash)
