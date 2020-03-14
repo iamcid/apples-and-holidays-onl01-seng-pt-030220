08e86751d39d5d66a2095d9dd9a31d51d8759f78
Christopher Cid <iamcid17@gmail.com>
2020-03-14 15:52:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,8 @@ def all_supplies_in_holidays(holiday_hash)
         array.each do |x|
           final_holiday << x.capitalize!
         end
-      holiday = final_holiday.join  
+      holiday = final_holiday.join(" ")  
+      
 end
 
 def all_holidays_with_bbq(holiday_hash)
